1d84a224c377ffbf235d96fc52b214057a146721
Estelle-Rose <serenetzen@gmail.com>
2021-03-11 11:20:05 +0100

    update commit

--- sendemail.php
+++ sendemail.php
@@ -4,40 +4,69 @@
 
 // Define some constants
 
-define("RECIPIENT_NAME", "Estelle Rose");
-define("RECIPIENT_EMAIL", "contact.aranea@gmail.com");
+
+$from = 'info@estelle-rose.fr';
+$sendTo = 'contact.aranea@gmail.com';
 
 
+// message that will be displayed when everything is OK :)
+$okMessage = 'Votre pigeon voyageur est en chemin, à bientôt';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'Il y a eu une erreur lors de l\'envol du pigeon voyageur, merci de réessayer ultérieurement';
 
 // Read the form values
 
-$success = false;
-$name = isset($_POST['nom']) ? preg_replace("/[^\.\-\' a-zA-Z0-9]/", "", $_POST['nom']) : "";
+
+$name = isset($_POST['nom']) ? preg_replace("/[^\.\-\' A-zÀ-ú0-9]/", "", $_POST['nom']) : "";
 $senderEmail = isset($_POST['email']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email']) : "";
-$objet = isset($_POST['objet']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['objet']) : "";
+$objet = isset($_POST['objet']) ? preg_replace("/[^\.\-\' A-zÀ-ú0-9]/", "", $_POST['objet']) : "";
 $message = isset($_POST['message']) ? preg_replace("/(From:|To:|BCC:|CC:|Objet:|Content-Type:)/", "", $_POST['message']) : "";
 
 $mail_objet = 'Un message de  ' . $name;
-$body = 'Name: ' . $name .  "\r\n";
+$body = 'Nom: ' . $name .  "\r\n";
 $body .= 'Email: ' . $senderEmail . "\r\n";
 
 if ($objet) {
-  $body .= 'objet: ' . $objet . "\r\n";
+  $body .= 'Objet: ' . $objet . "\r\n";
 }
 
 
-$body .= 'message: ' . "\r\n" . $message;
+$body .= 'Message: ' . "\r\n" . $message;
 
 // If all values exist, send the email
+try {
+  if ($name  && $senderEmail && $message) {
+  
+  // All the necessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from ,
+        'Reply-To: ' . $senderEmail,        
+        'Return-Path: ' . $from,
+    );
 
-if ($name  && $senderEmail && $message) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $name . " <" . $senderEmail . ">";
-  $success = mail($recipient, $mail_objet, $body, $headers);
-  echo "<div class='inner success '><p class='success'>Merci de nous avoir contacter, nous vous répondrons très bientôt !</p></div><!-- /.inner -->";
+  // send email
+  mail($sendTo, $mail_objet, $body, implode("\n", $headers));
+  $responseArray = array('type' => 'success', 'message' => $okMessage);
+  //echo "<div class='inner success '><p class='success'>Merci de nous avoir contacter, nous vous répondrons très bientôt !</p></div><!-- /.inner -->";
 
-} else {
-  echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
-
+} 
+}
+catch (\Exception $e) {
+  $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
